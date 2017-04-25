0ccda431f0bd39575e4134b72858d6ed14ac606c
sethvincent <sethvincent@gmail.com>
2017-04-25 16:36:48 -0400

    add cordova-react-dom prototype and modifications to react-native prototype

--- prototypes/react-native/FieldDataCollection/webpack.haul.js
+++ prototypes/react-native/FieldDataCollection/webpack.haul.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = ({ platform }, defaults) => ({
   entry: `./index.${platform}.js`,
   resolve: {
@@ -6,5 +8,39 @@ module.exports = ({ platform }, defaults) => ({
     alias: {
       fs: 'react-native-level-fs'
     }
-  }
+  },
+  module: {
+    ...defaults.module,
+    rules: [
+      ...defaults.module.rules,
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react-native']
+          }
+        }
+      },
+      {
+        loader: 'transform-loader?brfs',
+        enforce: 'post'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        transforms: [
+          function (file) {
+            return through(function (buf) {
+              this.queue(buf.split('').map(function (s) {
+                return String.fromCharCode(127 - s.charCodeAt(0));
+              }).join(''));
+            }, function () { this.queue(null); });
+          }
+        ]
+      }
+    })
+  ]
 });
